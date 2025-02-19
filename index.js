1b238402955026c9c8f0c7f0cab6f8e70793a678
Edgardo N. Angeles <angelesedgardo17@gmail.com>
2025-02-19 17:07:51 +0800

    FFFFF

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const mysql = require("mysql2");
-const { ethers } = require("ethers");
+
 
 const app = express();
 const PORT = 3000;
